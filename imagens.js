b996311ba69cb35fbf2f7d19b82fbd05d197bbad
Saulo01 <35872983+Saulo01@users.noreply.github.com>
2023-11-01 11:14:15 -0300

    Add files via upload

---
+++ imagens.js
@@ -0,0 +1,28 @@
+//imagens e sons do jogo
+
+let imagemDaEstrada;
+let imagemDoAtor;
+let imagemCarro;
+let imagemCarro2;
+let imagemCarro3;
+
+//sons do jogo
+
+let somDaTrilha;
+let somDaColisao;
+let somDoPonto;
+
+function preload(){
+  
+imagemDaEstrada = loadImage("imagem/estrada.png");
+imagemDoAtor = loadImage("imagem/ator-1.png");
+imagemCarro = loadImage("imagem/carro-1.png");
+imagemCarro2 = loadImage("imagem/carro-2.png");
+imagemCarro3 = loadImage("imagem/carro-3.png");
+imagemCarros = [imagemCarro,imagemCarro2,imagemCarro3,imagemCarro,imagemCarro2,imagemCarro3];
+  
+  somDaTrilha = loadSound("sons/trilha.mp3");
+  somDaColisao = loadSound("sons/colidiu.mp3");
+  somDoPonto = loadSound("sons/pontos.wav")
+
+}
